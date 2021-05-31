15dc74f3724c70279382cdcb62b323fabfd00469
Jussi Pohjolainen <jussi.pohjolainen@gmail.com>
2021-05-31 11:02:54 +0300

    delete customer working

--- src/main/java/fi/company/CustomerResource.java
+++ src/main/java/fi/company/CustomerResource.java
@@ -1,29 +1,44 @@
 package fi.company;
 
 import javax.annotation.PostConstruct;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Singleton;
+import javax.ws.rs.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Path("/customers")
+@ApplicationScoped
 public class CustomerResource {
 
     List<Customer> customers;
 
     @PostConstruct
     public void init() {
+        System.out.println("INIT!");
         customers = new ArrayList<>();
         customers.add(new Customer(1, "Jack"));
         customers.add(new Customer(2, "Tina"));
         customers.add(new Customer(3, "Hannah"));
-
     }
 
+
+    @DELETE
+    @Path("/{id}")
+    @Produces("application/json")
+    public String deleteCustomer(@PathParam("id") int id) {
+        List<Customer> temp = customers.stream().filter(cust -> cust.getId() != id).collect(Collectors.toList());
+        System.out.println(temp);
+        if(temp.size() != customers.size()) {
+            this.customers = temp;
+            return "{ \"status\": \"HTTP DELETE with id = " + id + "\" }";
+        } else {
+            return "{\"status\": \"not found\"}";
+        }
+    }
 
     @GET
     @Path("/{id}")
@@ -40,6 +55,8 @@ public class CustomerResource {
     @GET
     @Produces("application/json")
     public String getDateInHtml() {
+        System.out.println(customers);
+
         return customers.toString();
     }
 
--- src/main/java/fi/company/Main.java
+++ src/main/java/fi/company/Main.java
@@ -1,5 +1,6 @@
 package fi.company;
 
+import javax.inject.Singleton;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
